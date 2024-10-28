1a81b0155f50e7aef95a898506395da64d22a437
thallheim <thall@thallheim.com>
2024-10-28 01:44:03 +0100

    Import statements generated by first 'just' call.
    Super slow on Windwows, for some reason. Suspect it's the cmake script
    step. Needs looking into.

--- configure_justfile.cmake
+++ configure_justfile.cmake
@@ -1,12 +1,16 @@
 cmake_minimum_required(VERSION 3.25)
 
 string(TOLOWER "${CMAKE_HOST_SYSTEM_NAME}" HOST)
-set(CONFIG_IMPORT_STMNT "${CMAKE_SOURCE_DIR}/src/config_${HOST}.just")
+set(CONFIG_IMPORT_STMNT "import 'src/config_${HOST}.just'")
 set(JUSTFILE_PATH "${CMAKE_SOURCE_DIR}/justfile")
 
 file(READ ${JUSTFILE_PATH} JUSTFILE_CONTENT)
 
-string(REPLACE "@CONFIG_IMPORT_STMNT@" "${CONFIG_IMPORT_STMNT}" JUSTFILE_CONTENT "${JUSTFILE_CONTENT}")
+string(REPLACE "#CONFIG_IMPORT_STMNT#" "${CONFIG_IMPORT_STMNT}" JUSTFILE_CONTENT "${JUSTFILE_CONTENT}")
+# Write the build justfile's import statement
+string(REPLACE "#BUILD_IMPORT_STMT#"
+  "import? 'build/build.just'"
+  JUSTFILE_CONTENT "${JUSTFILE_CONTENT}")
 # Comment out the `configure-just` recipe to hide it after first run
 string(REPLACE "[no-exit-message]\n@configure-just:\n    cmake -P \"configure_justfile.cmake\"\n"
   "#[no-exit-message]\n#@configure-just:\n#   cmake -P \"configure_justfile.cmake\"\n"
--- justfile
+++ justfile
@@ -1,12 +1,8 @@
 set windows-shell := ["pwsh.exe","-NoLogo","-Command"]
 OS := os()
-# TODO: Can the build.just import be commented out and templated back
-# by the main config step instead, to avoid having to check for it
-# here? It's not going to exist for at least the first two just calls,
-# unless specific recipes are called by name.
-import? 'build/build.just'
-import? '@CONFIG_IMPORT_STMNT@'
 
+#BUILD_IMPORT_STMT#
+#CONFIG_IMPORT_STMNT#
 
 [no-exit-message]
 _default:
