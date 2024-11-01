83995892e32bea7a1af974446b0eb6457b06bfa8
thallheim <thall@thallheim.com>
2024-11-01 11:05:52 +0100

    Arg checking simplified

--- pwsh-testing/oi.ps1
+++ pwsh-testing/oi.ps1
@@ -2,28 +2,45 @@ $nl        = "`r`n" # Because backticking on my kb layout is ass
 $pipeInput = ($input | Out-String).Trim() # Stringify any piped input
 $argsInput =  $args
 
-function is_single_arg { 
+# Only supports a single input arg, so make sure there aren't more
+function check_args {
     switch ($argsInput.Count) {
         0       { return $false }
         1       { return $true  }
         default { return $false }
     }
 }
+$argsOk = check_args
+Write-Debug "argsOk = ${argsOk}"
+
+
+# function check_pipe {
+#     if (-not [string]::IsNullOrEmpty($pipeInput)) {
+#         return $true
+#     } else {
+#         return $false
+#     }
+# }
+
+$is_pipe = if (-not [string]::IsNullOrEmpty($pipeInput)) {
+    $true
+} else {
+    $false
+}
 
 # Ensure EITHER piped or arg input; error if both are provided
-if ($argsInput.Count -gt 0 -and -not [string]::IsNullOrEmpty($pipeInput)) {
+# if ($argsInput.Count -gt 0 -and -not [string]::IsNullOrEmpty($pipeInput)) {
+if ($argsInput.Count -gt 0 -and $is_pipe) {
     Write-Error "Can't handle simultaneous pipe and arg input."
     exit 1
 }
 
-# Ensure singular input arg
-$argsInputOk = is_single_arg
 
-if (argsInputOk) {
-    $count = ($argsInput).Count
+if ($argsOk) {
+    $count = ($args).Count
     Write-Information -msg "Arg count: ${count}"
 } else {
-    $count = ($argsInput).Count
+    $count = ($args).Count
     Write-Error "Too many args (${count}). If providing a path, ensure it is quoted."
     exit 1
 }
