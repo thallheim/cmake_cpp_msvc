b68d23950fac56130e622de830443735207f1b38
thallheim <thall@thallheim.com>
2024-11-01 13:31:06 +0100

    Better

--- pwsh-testing/oi.ps1
+++ pwsh-testing/oi.ps1
@@ -2,47 +2,48 @@ $nl        = "`r`n" # Because backticking on my kb layout is ass
 $pipeInput = ($input | Out-String).Trim() # Stringify any piped input
 $argsInput =  $args
 
-# Only supports a single input arg, so make sure there aren't more
+
 function check_args {
     switch ($argsInput.Count) {
         0       { return $false }
         1       { return $true  }
-        default { return $false }
+        default {
+            $count = ($argsInput).Count
+            Write-Error "Too many args (${count}). Paths with spaces need quoting."
+            return $false }
     }
 }
-$argsOk = check_args
-Write-Debug "argsOk = ${argsOk}"
+
+$argsOk = $false
 
 
-# function check_pipe {
-#     if (-not [string]::IsNullOrEmpty($pipeInput)) {
-#         return $true
-#     } else {
-#         return $false
-#     }
-# }
-
-$is_pipe = if (-not [string]::IsNullOrEmpty($pipeInput)) {
+# Check for piped input (take precedence if arg also provided)
+$pipedInput = if (-not [string]::IsNullOrEmpty($pipeInput)) {
+    $argsOk = check_args
+    Write-Information -msg "pipedInput = ${pipedInput}"
     $true
 } else {
+    $argsOk = check_args
+    Write-Information -msg "argsOk = ${argsOk}"
     $false
 }
 
+
+
 # Ensure EITHER piped or arg input; error if both are provided
 # if ($argsInput.Count -gt 0 -and -not [string]::IsNullOrEmpty($pipeInput)) {
-if ($argsInput.Count -gt 0 -and $is_pipe) {
+if ($argsOk -and $pipedInput) {
     Write-Error "Can't handle simultaneous pipe and arg input."
     exit 1
 }
 
 
 if ($argsOk) {
-    $count = ($args).Count
-    Write-Information -msg "Arg count: ${count}"
-} else {
-    $count = ($args).Count
-    Write-Error "Too many args (${count}). If providing a path, ensure it is quoted."
+    Write-Error "OH NOES ARGS"
     exit 1
 }
 
-# Write-Debug -msg "Processesing piped input:" -InformationAction Continue
+if ($pipedInput) {
+    Write-Error "OH NOES PIPE"
+    exit 1
+}
