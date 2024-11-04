2db623981b6b1ad62898c949373f114752022c96
thallheim <thall@thallheim.com>
2024-11-05 00:28:40 +0100

    Add dir for support scriptery

--- .support_scripts/configure_justfile.cmake
+++ .support_scripts/configure_justfile.cmake
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.25)
 
 string(TOLOWER "${CMAKE_HOST_SYSTEM_NAME}" HOST)
-set(CONFIG_IMPORT_STMNT "import 'src/config_${HOST}.just'")
+set(CONFIG_IMPORT_STMNT "import '.support_scripts/config_${HOST}.just'")
 set(JUSTFILE_PATH "${CMAKE_SOURCE_DIR}/justfile")
 
 file(READ ${JUSTFILE_PATH} JUSTFILE_CONTENT)
@@ -12,8 +12,8 @@ string(REPLACE "#BUILD_IMPORT_STMT#"
   "import? 'build/build.just'"
   JUSTFILE_CONTENT "${JUSTFILE_CONTENT}")
 # Comment out the `configure-just` recipe to hide it after first run
-string(REPLACE "[no-exit-message]\n@configure-just:\n    cmake -P \"backup_unconfigured_justfile.cmake\"\n    cmake -P \"configure_justfile.cmake\"\n"
-  "#[no-exit-message]\n#@configure-just:\n#   cmake -P \"backup_unconfigured_justfile.cmake\"\n#    cmake -P \"configure_justfile.cmake\"\n"
+string(REPLACE "[no-exit-message]\n@configure-just:\n    cmake -P \".support_scripts/backup_unconfigured_justfile.cmake\"\n    cmake -P \".support_scripts/configure_justfile.cmake\"\n"
+  "#[no-exit-message]\n#@configure-just:\n#   cmake -P \".support_scripts/backup_unconfigured_justfile.cmake\"\n#    cmake -P \".support_scripts/configure_justfile.cmake\"\n"
   JUSTFILE_CONTENT "${JUSTFILE_CONTENT}")
 # Switch default recipe from `config-just` to `_fuzzy-list`
 string(REPLACE "[no-exit-message]\n_default:\n    @just configure-just\n"
