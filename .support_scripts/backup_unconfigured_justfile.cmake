bcdad456b115c0b39ecd915264e990fbd991c040
thallheim <thall@thallheim.com>
2024-11-06 01:02:06 +0100

    FIX: Root justfile now only backed up if it doesn't already exist
    README updated to reflect

--- .support_scripts/backup_unconfigured_justfile.cmake
+++ .support_scripts/backup_unconfigured_justfile.cmake
@@ -1,6 +1,12 @@
 cmake_minimum_required(VERSION 3.25)
 
 set(JUSTFILE_PATH "${CMAKE_SOURCE_DIR}/justfile")
+set(BACKUP_PATH "${CMAKE_BINARY_DIR}/build/unconfigured.just")
 
-file(READ ${JUSTFILE_PATH} JUSTFILE_CONTENT)
-file(WRITE ${CMAKE_BINARY_DIR}/build/unconfigured.just "${JUSTFILE_CONTENT}")
+if(EXISTS ${BACKUP_PATH})
+  message(STATUS "Skip root justfile backup - backup exists")
+else()
+  file(READ ${JUSTFILE_PATH} JUSTFILE_CONTENT)
+  file(WRITE ${CMAKE_BINARY_DIR}/build/unconfigured.just "${JUSTFILE_CONTENT}")
+  message(STATUS "Backup (unconfigured) root justfile created at: '${BACKUP_PATH}'")
+endif()
